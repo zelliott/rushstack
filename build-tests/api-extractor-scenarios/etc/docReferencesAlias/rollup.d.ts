3d0625f9b057e474a07f3f5d1be65a7d571fdad4
Frank Wienberg <frank.wienberg@coremedia.com>
2022-08-26 13:22:19 +0200

    [api-extractor] Add scenario for #3593, incorrect references to renamed classes
    "Incorrect canonical reference to aliased class in .api.json"
    https://github.com/microsoft/rushstack/issues/3593
    
    Interface "Options" is renamed to "OptionsRenamed" via
    `export { __ as __ }`. This works consistently for *.api.md
    and rollup.d.ts as well as the "name" in *.api.json, but not
    for the several other references in *.api.json (see git diff
    after running api-extractor-scenarios).

---
+++ build-tests/api-extractor-scenarios/etc/docReferencesAlias/rollup.d.ts
@@ -0,0 +1,12 @@
+/** @public */
+export declare class Item {
+    options: OptionsRenamed;
+}
+
+/** @public */
+export declare interface OptionsRenamed {
+    name: string;
+    color: 'red' | 'blue';
+}
+
+export { }
